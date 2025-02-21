48d6be66b18674ccdf89478eb7cbec8084022713
valelectronic <nnanwikevictor@gmail.com>
2025-02-21 08:32:43 +0800

    Fixed server port log issue

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json())
 
 app.use("/api/Auth", AuthRoutes)
 
-const PORT = process.env.PORT || 6000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, ()=>{
     console.log(` server is running on port ${PORT}`)
