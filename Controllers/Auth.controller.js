f6df3c0f52d4d756c7a69f2769157d7b6d0ce447
valelectronic <nnanwikevictor@gmail.com>
2025-02-22 12:12:32 +0800

    improved

--- Controllers/Auth.controller.js
+++ Controllers/Auth.controller.js
@@ -1,6 +1,6 @@
 export const AuthUsers = (req, res) =>{
     res.json([
-        {id: 1, name: "victor"},
+        {id: 1, name: "victor valentine odo 2"},
         {id:2 , name: 'valentine'}
     ])
 }
